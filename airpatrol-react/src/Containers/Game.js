4e70acfdd0cacf58a3b084d9de115d9b640f4282
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-07-04 00:21:40 -0400

    useEffect for starting the game

--- airpatrol-react/src/Containers/Game.js
+++ airpatrol-react/src/Containers/Game.js
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux'
+import { assignAtmosphere } from '../actionCreator'
+
 
 function Game(props) {
+
+    useEffect(() => {
+        fetch("http://localhost:3000/atmospheres", {
+            method: "Post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ user_id: props.currentUser.id })
+        })
+            .then(resp => resp.json())
+            .then(response => props.assignAtmosphere(response))
+    }, [])
+
+
     return (
         <div>
-            <h2>GAMEEEE!</h2>
-            <h2>GAMEEEE!</h2>
-            <h2>GAMEEEE!</h2>
-            <h2>GAMEEEE!</h2>
-            <h2>GAMEEEE!</h2>
-            <h2>GAMEEEE!</h2>
-            <h2>GAMEEEE!</h2>
+            {props.atmosphere ?
+                <div>
+                    <h2>{props.currentUser.name}</h2>
+                    <h2>{props.atmosphere.id}</h2>
+                    <h2>{props.trees.length}</h2>
+                    <h2>{props.score}</h2>
+                </div>
+                : null}
         </div>
     )
 }
 
-export default Game
+const msp = state => {
+    return {
+        score: state.score,
+        trees: state.trees,
+        atmosphere: state.atmosphere,
+        currentUser: state.currentUser,
+    }
+}
+
+export default connect(msp, { assignAtmosphere })(Game)
