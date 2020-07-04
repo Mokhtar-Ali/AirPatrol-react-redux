d79425bb403160cd80c300147cccbde9501fbc5b
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-07-03 23:39:18 -0400

    implemented auto_login

--- airpatrol-react/src/App.js
+++ airpatrol-react/src/App.js
@@ -1,18 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
 import './App.css';
 import { connect } from 'react-redux'
 import Instructions from './Components/Instructions'
 import Signup from './Forms/Signup';
 import Login from './Forms/Login';
+import { assignUser } from './actionCreator'
 
 function App(props) {
   const [switchLoginToSignup, setswitchLoginSignup] = useState(false)
+  const history = useHistory()
+
+  useEffect(() => {
+    const user_id = localStorage.user_id;
+    if (user_id) {
+      fetch("http://localhost:3000/auto_login", {
+        headers: {
+          Authorization: user_id
+        }
+      })
+        .then(resp => resp.json())
+        .then(response => {
+          if (response.errors) {
+            alert(response.errors);
+          } else {
+            props.assignUser(response);
+          }
+        });
+    }
+  })
+
 
   return (
     <div className="App">
       <Instructions />
       {props.currentUser ?
-        <button> Start Game </button>
+        <button onClick={()=> history.push('/game')}> Start Game </button>
         : <div>
           {switchLoginToSignup ? <Signup /> : <Login />}
           <button onClick={() => setswitchLoginSignup(!switchLoginToSignup)}>Switch to Signup</button>
@@ -29,4 +52,4 @@ const msp = state => {
   }
 }
 
-export default connect(msp)(App)
+export default connect(msp, {assignUser})(App)
