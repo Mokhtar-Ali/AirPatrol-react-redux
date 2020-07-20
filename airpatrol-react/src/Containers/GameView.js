51d506a1cf7bd50fcc6dd2d9bab04ff3957e98b2
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-07-20 15:41:38 -0400

    designed the divs with style names for GameView

--- airpatrol-react/src/Containers/GameView.js
+++ airpatrol-react/src/Containers/GameView.js
@@ -8,7 +8,20 @@ class GameView extends React.Component {
     render() {
         return (
             <div className='game-view' >
-          
+                <div className='sky'> Sky </div>
+                <div className='game-data'>
+                    <div className='left-side'> left-side</div>
+                    <div className='habitation'>
+                        <div className='green-space'>green-space</div>
+                        <div className='person-space'>
+                            <div className='health-bar'>health-bar</div>
+                            <div className='person'> person</div>
+                            <div className='fire'>fire</div>
+                            <div className='fire-health'>fire-health</div>
+                        </div>
+                    </div>
+                    <div className='right-side'>Trees</div>
+                </div>
             </div>
         )
     }
