dfc7ef2bff74111ea839d923b33a89f21a6b3c3c
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-08-04 18:55:26 -0400

    added well, tent, fire, firewood

--- airpatrol-react/src/Containers/GameView.js
+++ airpatrol-react/src/Containers/GameView.js
@@ -2,16 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux'
 import '../Css/gameView.css'
 import Tree from '../Components/Tree'
+import Well from '../images/Well.png'
+import Tent from '../images/Tent.png'
+import Fire from '../images/Fire.png'
+import Firewood from '../images/Firewood.png'
 
 
 class GameView extends React.Component {
 
+    renderFireWoood = () => {
+        let fireWood = this.props.fireWood
+        
+    }
 
     render() {
         return (
             <div className='game-view' >
                 <div className='trees'>
-                    {this.props.trees.slice(0,38).map(tree =>
+                    {this.props.trees.slice(0, 38).map(tree =>
                         <Tree
                             tree={tree}
                             key={tree.id}
@@ -19,10 +27,18 @@ class GameView extends React.Component {
                     )}
                 </div>
                 <div className='data'>
-                    <div className='well'></div>
-                    <div className='person'></div>
-                    <div className='fire'></div>
-                    <div className='fire-wood'></div>
+                    <div className='well-container'>
+                        <img className='well' src={Well} />
+                    </div>
+                    <div className='person'>
+                        <img className='tent' src={Tent} />
+                    </div>
+                    <div className='fire-container'>
+                        <img className='fire' src={Fire} />
+                    </div>
+                    <div className='fire-wood-container'>
+                            <img className='fire-wood' src={Firewood} />
+                    </div>
                 </div>
             </div>
         )
@@ -31,7 +47,8 @@ class GameView extends React.Component {
 
 const msp = (state) => {
     return {
-        trees: state.trees
+        trees: state.trees,
+        fireWood: state.fireWood
     }
 }
 export default connect(msp)(GameView)
